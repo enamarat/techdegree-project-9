0233fd2ce769b22644dfbee14024b7f90ce698e7
Marat Enaliev <enamarat@yandex.ru>
2019-03-07 20:17:39 +0300

    Make a config.js file.

--- components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Header = () => {
-  return (
-    <header>
-      <h1> Image Gallery </h1>
-    </header>
-  );
-}
--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Header from './Header.js'
 import Gallery from './Gallery.js'
+import apiKey from './config.js'
+
+console.log(apiKey)
 
 class App extends React.Component {
   /* Initial state reflects the fact that there are no images*/
